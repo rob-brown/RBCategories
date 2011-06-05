65bcb095b1dde22b3163b18d9f4819dd2211dd9a
Robert Brown <ammoknight@gmail.com>
2011-06-04 23:27:18 -0600

    Added the ability to request the MIME type of a file path stored in an NSString.

--- NSString+RBExtras.h
+++ NSString+RBExtras.h
@@ -24,13 +24,36 @@
 
 + (NSString *)stringWithLatitude:(double)latitude longitude:(double)longitude;
 
+/**
+ * Returns a string with all instances of the given characters removed from the 
+ * receiver. The receiver itself is unaffected.
+ *
+ * @return A string stripped of the given characters.
+ */
 - (NSString *)stringByRemovingCharactersInString:(NSString *)characters;
 
 
 //------------------------------------------------------------------------------
-// Path creation methods.
+// Path methods.
 //------------------------------------------------------------------------------
 
+/**
+ * Given an array of path components relative to the documents directory, 
+ * constructs and returns a path.
+ *
+ * @param components An array of NSStrings which make up the components in a 
+ * path relative to the documents directory.
+ *
+ * @return A string represting a file path. 
+ */
 + (NSString *)pathWithComponentsRelativeToDocumentsDirectory:(NSArray *)components;
+
+/**
+ * Returns the MIME type of the string assuming it's a valid path. The return 
+ * type is undefined if the string is not in a valid path format.
+ *
+ * @return The MIME type of the string.
+ */
+- (NSString *)MIMEType;
 
 @end
